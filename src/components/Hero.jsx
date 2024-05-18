f7b20b67428cb5503c2ec40a313d5451a126a9d1
Khai Pong Teoh <khaipongteoh@gmail.com>
2024-05-18 14:11:04 +0100

    Finish Body Content

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,14 +6,20 @@ const Hero = () => {
         <div className="flex flex-wrap">
             <div className="w-full lg:w-1/2">
                 <div className="flex flex-col items-center lg-items-start">
-                    <h1 className="pb-16 text-6xl font-thin tracking-tighter lg:mt-16 lg:text-8xl">Khai Pong Teoh</h1>
-                    <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Looking for Software Engineer Role</span>
-                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
+                    <h1 className="pb-16 text-6xl font-thin tracking-tighter lg:mt-16 lg:text-8xl">
+                        Khai Pong Teoh
+                    </h1>
+                    <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">
+                        Looking for Software Engineer Role
+                    </span>
+                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+                        {HERO_CONTENT}
+                    </p>
                 </div>
             </div>
             <div className="w-full lg:w-1/2 lg:p-8">
                 <div className="flex justify-center">
-                    <img src="/kp-logo.png" alt="KP Teoh" />
+                    <img src="" alt="KP Teoh" />
                 </div>
             </div>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { MdEmail } from "react-icons/md";
 const Navbar = () => {
   return <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className="h-20 w-25" src="/kp-logo.png" alt="" />
+            <img className="h-40 w-30" src="/kp-logo.png" alt="" />
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <FaLinkedin />
