6b901b1d99bc22f43b3445439450f45d2de42688
Khai Pong Teoh <khaipongteoh@gmail.com>
2025-04-15 23:24:56 +0800

    Add New Project

--- src/components/section/Projects.jsx
+++ src/components/section/Projects.jsx
@@ -20,7 +20,15 @@ export const Projects = () => {
                 monitoring and automated scaling.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {[ "React", "Next.js", "TypeScript","Node.js", "Express.js", "AWS", "Tailwind CSS"].map((tech, key) => (
+                {[
+                  "React",
+                  "Next.js",
+                  "TypeScript",
+                  "Node.js",
+                  "Express.js",
+                  "AWS",
+                  "Tailwind CSS",
+                ].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
@@ -56,22 +64,27 @@ export const Projects = () => {
                 integration, and customizable product inventory.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Next.js", "JavaScript", "Tailwind CSS", "MongoDB", "Clerk", "Inngest"].map(
-                  (tech) => (
-                    <span
-                      key={tech}
-                      className="
+                {[
+                  "Next.js",
+                  "JavaScript",
+                  "Tailwind CSS",
+                  "MongoDB",
+                  "Clerk",
+                  "Inngest",
+                ].map((tech) => (
+                  <span
+                    key={tech}
+                    className="
                       bg-blue-500/10 text-blue-500 py-1 px-3 
                       rounded-full text-sm
                       transition
                       hover:bg-blue-500/20 hover:-translate-y-0.5
                       hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
                     "
-                    >
-                      {tech}
-                    </span>
-                  )
-                )}
+                  >
+                    {tech}
+                  </span>
+                ))}
               </div>
               <div className="flex justify-between items-center">
                 <a
@@ -93,7 +106,47 @@ export const Projects = () => {
             >
               <h3 className="text-xl font-bold mb-2">React Movie App</h3>
               <p className="text-gray-400 mb-4">
-              Built with React.js for the user interface, Appwrite for the Trending Movies Algorithm, and styled with TailwindCSS
+                Built with React.js for the user interface, Appwrite for the
+                Trending Movies Algorithm, and styled with TailwindCSS
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["React", "Tailwind CSS", "Appwrite"].map((tech, key) => (
+                  <span
+                    key={key}
+                    className="
+                      bg-blue-500/10 text-blue-500 py-1 px-3 
+                      rounded-full text-sm
+                      transition
+                      hover:bg-blue-500/20 hover:-translate-y-0.5
+                      hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)]
+                    "
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-between items-center">
+                <a
+                  href="#"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  View Project →
+                </a>
+              </div>
+            </div>
+
+            <div
+              className="
+              glass p-6 rounded-xl border border-white/10 
+              hover:-translate-y-1 hover:border-blue-500/30
+              hover:shadow-[0_4px_20px_rgba(59,130,246,0.1)]
+              transition-all
+            "
+            >
+              <h3 className="text-xl font-bold mb-2">React Movie App</h3>
+              <p className="text-gray-400 mb-4">
+                Built with React.js for the user interface, Appwrite for the
+                Trending Movies Algorithm, and styled with TailwindCSS
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
                 {["React", "Tailwind CSS", "Appwrite"].map((tech, key) => (
